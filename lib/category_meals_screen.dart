3ebca6482dee012d5dbed7d29aa39e88ad7e4377
Tbrahn <tbranham9323@gmail.com>
2020-08-22 20:13:07 -0400

    add meal class constructor

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -18,11 +18,9 @@ class CategoryMealsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text(
-          'The Recipes for this',
-        ),
-      ),
+      body: ListView.builder(itemBuilder: (ctx, index) {
+        
+      },itemCount: ,),
     );
   }
 }
