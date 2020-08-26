04b7d17d09c855102375160d2d03185ab91a9803
Tbrahn <tbranham9323@gmail.com>
2020-08-26 03:15:29 -0400

    add meal item widget

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../dummy_data.dart';
+import '../widgets/meal_item.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = 'category-meals';
+  // final String categoryID;
+  // final String cetegoryTitle;
+
+  // CategoryMealsScreen(this.categoryID, this.cetegoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryID = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryID);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            duration: categoryMeals[index].duration,
+            affordability: categoryMeals[index].affordability,
+            complexity: categoryMeals[index].complexity,
+          );
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
