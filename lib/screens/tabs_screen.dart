736b50658faee0b94f1bc9ea1a16042af6f21969
Tbrahn <tbranham9323@gmail.com>
2020-09-06 03:12:47 -0400

    add tabs and favorites screen

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import './categories_screen.dart';
+import './favorites_screen.dart';
+
+class TabScreen extends StatefulWidget {
+  @override
+  _TabScreenState createState() => _TabScreenState();
+}
+
+class _TabScreenState extends State<TabScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Meals'),
+          bottom: TabBar(tabs: <Widget>[
+            Tab(
+              icon: Icon(
+                Icons.category,
+              ),
+              text: 'Categories',
+            ),
+            Tab(
+              icon: Icon(
+                Icons.star,
+              ),
+              text: 'Favorite',
+            ),
+          ]),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            CategoriesScreen(),
+            FavoritesScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}
