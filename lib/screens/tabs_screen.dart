95aff18f75ead26950a34e3b367afd050feb1312
Tbrahn <tbranham9323@gmail.com>
2020-09-15 03:22:49 -0400

    add bottom navigation tabs

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -9,34 +9,47 @@ class TabScreen extends StatefulWidget {
 }
 
 class _TabScreenState extends State<TabScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Your Favorite',
+    },
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(tabs: <Widget>[
-            Tab(
-              icon: Icon(
-                Icons.category,
-              ),
-              text: 'Categories',
-            ),
-            Tab(
-              icon: Icon(
-                Icons.star,
-              ),
-              text: 'Favorite',
-            ),
-          ]),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text("Categories"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            title: Text("Favorites"),
+          )
+        ],
       ),
     );
   }
