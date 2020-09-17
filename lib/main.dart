95cde26a129efc1b4389cc192dfd3e9234160049
Tbrahn <tbranham9323@gmail.com>
2020-09-17 02:38:30 -0400

    add filters screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:Meals_App/screens/filters_screen.dart';
 import 'package:flutter/material.dart';
 
 import './screens/meal_detail_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
+import './screens/filters_screen.dart';
 import './screens/tabs_screen.dart';
 
 void main() => runApp(MyApp());
@@ -36,6 +38,7 @@ class MyApp extends StatelessWidget {
         '/': (ctx) => TabScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(),
       },
       onGenerateRoute: (settings) {
         print(settings.arguments);
--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../screens/filters_screen.dart';
+
 class MainDrawer extends StatelessWidget {
-  Widget buildListTile(String title, IconData icon) {
+  Widget buildListTile(String title, IconData icon, Function tapHandler) {
     return ListTile(
-            leading: Icon(
-              icon,
-              size: 26,
-            ),
-            title: Text(
-              title,
-              style: TextStyle(
-                fontFamily: 'RobotoCondensed',
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            onTap: () {
-              //..
-            },
-          ),
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: tapHandler,
+    );
   }
 
   @override
@@ -44,8 +44,12 @@ class MainDrawer extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          buildListTile('Meals', Icons.restaurant,),
-          buildListTile('Filters', Icons.settings,),
+          buildListTile('Meals', Icons.restaurant, () {
+            Navigator.of(context).pushReplacementNamed('/');
+          }),
+          buildListTile('Filters', Icons.settings, () {
+            Navigator.of(context).pushReplacementNamed(FiltersScreen.routeName);
+          }),
         ],
       ),
     );
